8eae8aac88c74bd190f73b12e7de2fa91cd6a450
wndudrla1011 <wndudrla1011@naver.com>
2024-03-02 18:04:24 +0900

    Initial commit

---
+++ src/test/java/com/rootable/mallmarkme2024/MallMarkme2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootable.mallmarkme2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMarkme2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
