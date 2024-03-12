f52de3bfff12bfc2cbcc7d490df19af31b942cc9
wndudrla1011 <wndudrla1011@naver.com>
2024-03-12 12:44:16 +0900

    changed layout to MainLayout

--- src/main/frontend/src/pages/MainPage.js
+++ src/main/frontend/src/pages/MainPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import MainLayout from "../layouts/MainLayout";
 
 function MainPage() {
-  return <div className="text-3xl">Main Page</div>;
+  return (
+    <MainLayout>
+      <div className="text-3xl">Main Page</div>
+    </MainLayout>
+  );
 }
 
 export default MainPage;
