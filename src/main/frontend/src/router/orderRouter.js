5270f421b0c88aca1a5e31b059898b8934b5c015
wndudrla1011 <wndudrla1011@naver.com>
2024-03-14 16:20:36 +0900

    routing for order and replace index to list

---
+++ src/main/frontend/src/router/orderRouter.js
@@ -0,0 +1,33 @@
+import React, { Suspense, lazy } from "react";
+import Loading from "../pages/Loading";
+import { Navigate } from "react-router-dom";
+
+const OrderList = lazy(() => import("../pages/orders/ListPage"));
+const OrderRead = lazy(() => import("../pages/orders/ReadPage"));
+
+const orderRouter = () => {
+  return [
+    {
+      path: "list",
+      element: (
+        <Suspense fallback={<Loading />}>
+          <OrderList />
+        </Suspense>
+      ),
+    },
+    {
+      path: "",
+      element: <Navigate replace={true} to={"list"} />,
+    },
+    {
+      path: "read/:orderId",
+      element: (
+        <Suspense fallback={<Loading />}>
+          <OrderRead />
+        </Suspense>
+      ),
+    },
+  ];
+};
+
+export default orderRouter;
--- src/main/frontend/src/router/root.js
+++ src/main/frontend/src/router/root.js
@@ -2,16 +2,17 @@ import { Suspense, lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Loading from "../pages/Loading";
 import cartRouter from "./cartRouter";
+import orderRouter from "./orderRouter";
 
-// const Main = lazy(() => {
-//   return Promise.all([
-//     import("../pages/MainPage"),
-//     new Promise((resolve) => setTimeout(resolve, 1200)),
-//   ]).then(([moduleExports]) => moduleExports);
-// });
+const Main = lazy(() => {
+  return Promise.all([
+    import("../pages/MainPage"),
+    new Promise((resolve) => setTimeout(resolve, 1200)),
+  ]).then(([moduleExports]) => moduleExports);
+});
 
-const Main = lazy(() => import("../pages/MainPage"));
 const CartIndex = lazy(() => import("../pages/cart/IndexPage"));
+const OrderIndex = lazy(() => import("../pages/orders/IndexPage"));
 
 const root = createBrowserRouter([
   {
@@ -30,6 +31,15 @@ const root = createBrowserRouter([
       </Suspense>
     ),
     children: cartRouter(),
+  },
+  {
+    path: "order",
+    element: (
+      <Suspense fallback={<Loading />}>
+        <OrderIndex />
+      </Suspense>
+    ),
+    children: orderRouter(),
   },
 ]);
 
