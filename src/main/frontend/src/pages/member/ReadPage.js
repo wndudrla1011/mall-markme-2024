935157cf59ec0aab11e403fda62b71d340a194ba
wndudrla1011 <wndudrla1011@naver.com>
2024-03-14 18:22:44 +0900

    added edit button and function for moving and keep query string

--- src/main/frontend/src/pages/member/ReadPage.js
+++ src/main/frontend/src/pages/member/ReadPage.js
@@ -1,15 +1,37 @@
 import React from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import {
+  createSearchParams,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 
 function ReadPage() {
   const { memberId } = useParams();
+  const navigate = useNavigate();
 
   const [queryParams] = useSearchParams();
 
   const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
   const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
 
-  return <div className="text-3xl">Read Page {memberId}</div>;
+  const queryStr = createSearchParams({ page, size }).toString();
+
+  const moveToEdit = (memberId) => {
+    navigate({
+      pathname: `/member/edit/${memberId}`,
+      search: queryStr,
+    });
+  };
+
+  return (
+    <div className="text-3xl">
+      Read Page {memberId}
+      <div>
+        <button onClick={() => moveToEdit(memberId)}>수정</button>
+      </div>
+    </div>
+  );
 }
 
 export default ReadPage;
