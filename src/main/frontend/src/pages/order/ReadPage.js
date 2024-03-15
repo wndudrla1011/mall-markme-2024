b60986d5989438c3007ccc9e9957e87ff3d01969
wndudrla1011 <wndudrla1011@naver.com>
2024-03-15 11:51:45 +0900

    added edit button and got query string

--- src/main/frontend/src/pages/order/ReadPage.js
+++ src/main/frontend/src/pages/order/ReadPage.js
@@ -1,12 +1,37 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import {
+  createSearchParams,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 
 function ReadPage() {
   const { orderId } = useParams();
+  const navigate = useNavigate();
 
-  console.log(orderId);
+  const [queryParams] = useSearchParams();
 
-  return <div className="text-3xl">ReadPage</div>;
+  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
+  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
+
+  const queryStr = createSearchParams({ page, size });
+
+  const moveToEdit = (orderId) => {
+    navigate({
+      pathname: `/order/edit/${orderId}`,
+      search: queryStr,
+    });
+  };
+
+  return (
+    <div className="text-3xl">
+      Read Page {orderId}
+      <div>
+        <button onClick={() => moveToEdit(orderId)}>수정</button>
+      </div>
+    </div>
+  );
 }
 
 export default ReadPage;
