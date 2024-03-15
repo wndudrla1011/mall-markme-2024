fe49d412ef46ee46365c83183978a3e2311e8950
wndudrla1011 <wndudrla1011@naver.com>
2024-03-15 11:56:05 +0900

    removed edit button for moving

--- src/main/frontend/src/pages/order/ReadPage.js
+++ src/main/frontend/src/pages/order/ReadPage.js
@@ -1,37 +1,15 @@
 import React from "react";
-import {
-  createSearchParams,
-  useNavigate,
-  useParams,
-  useSearchParams,
-} from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 function ReadPage() {
   const { orderId } = useParams();
-  const navigate = useNavigate();
 
   const [queryParams] = useSearchParams();
 
   const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
   const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
 
-  const queryStr = createSearchParams({ page, size });
-
-  const moveToEdit = (orderId) => {
-    navigate({
-      pathname: `/order/edit/${orderId}`,
-      search: queryStr,
-    });
-  };
-
-  return (
-    <div className="text-3xl">
-      Read Page {orderId}
-      <div>
-        <button onClick={() => moveToEdit(orderId)}>수정</button>
-      </div>
-    </div>
-  );
+  return <div className="text-3xl">Read Page {orderId}</div>;
 }
 
 export default ReadPage;
