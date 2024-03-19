a3d4d0d8670a4962c885af225ddd6773e1b165e9
wndudrla1011 <wndudrla1011@naver.com>
2024-03-19 17:17:23 +0900

    added message for validation

--- src/main/java/com/rootable/mallmarkme2024/dto/ItemSaveDTO.java
+++ src/main/java/com/rootable/mallmarkme2024/dto/ItemSaveDTO.java
@@ -1,9 +1,14 @@
 package com.rootable.mallmarkme2024.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
@@ -17,14 +22,21 @@ public class ItemSaveDTO {
 
     private Long id;
 
+    @NotBlank(message = "상품명을 입력해주세요")
     private String name;
 
+    @NotBlank(message = "저자를 입력해주세요")
     private String writer;
 
+    @NotNull(message = "가격을 입력해주세요")
+    @Range(min = 1000, max = 100000)
     private Integer price;
 
     private String description;
 
+    @NotNull(message = "재고를 입력해주세요")
+    @PositiveOrZero
+    @Max(999)
     private Integer stock;
 
     private boolean isDeleted;
