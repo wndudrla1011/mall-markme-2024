5be405177e5feabd369152fe8127f6b5700a4cc3
wndudrla1011 <wndudrla1011@naver.com>
2024-03-19 12:16:56 +0900

    added logic to get list for Item and tested

--- src/main/java/com/rootable/mallmarkme2024/controller/ItemController.java
+++ src/main/java/com/rootable/mallmarkme2024/controller/ItemController.java
@@ -1,6 +1,8 @@
 package com.rootable.mallmarkme2024.controller;
 
 import com.rootable.mallmarkme2024.dto.ItemDTO;
+import com.rootable.mallmarkme2024.dto.PageRequestDTO;
+import com.rootable.mallmarkme2024.dto.PageResponseDTO;
 import com.rootable.mallmarkme2024.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -19,7 +21,21 @@ public class ItemController {
 
     @GetMapping("/{itemId}")
     public ItemDTO get(@PathVariable("itemId") Long itemId) {
+
+        log.info("상품 정보");
+
         return itemService.get(itemId);
+
+    }
+
+    @GetMapping("/list")
+    public PageResponseDTO<ItemDTO> list(PageRequestDTO pageRequestDTO) {
+
+        log.info("상품 목록");
+        log.info("list : " + pageRequestDTO);
+
+        return itemService.getList(pageRequestDTO);
+
     }
 
 }
--- src/test/java/com/rootable/mallmarkme2024/repository/ItemRepositoryTest.java
+++ src/test/java/com/rootable/mallmarkme2024/repository/ItemRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.rootable.mallmarkme2024.repository;
 
 import com.rootable.mallmarkme2024.domain.Item;
+import com.rootable.mallmarkme2024.dto.PageRequestDTO;
+import com.rootable.mallmarkme2024.service.ItemService;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import java.util.UUID;
+
 @SpringBootTest
 @Log4j2
 public class ItemRepositoryTest {
@@ -17,21 +21,28 @@ public class ItemRepositoryTest {
     @Autowired
     private ItemRepository itemRepository;
 
+    @Autowired
+    private ItemService itemService;
+
     @Test
     @DisplayName("샘플 데이터 생성용")
     public void sample() throws Exception {
 
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 5; i++) {
             Item item = Item.builder()
-                    .name("Item " + i)
+                    .name("Item " + (i + 20))
                     .price(i * 1000)
                     .stock(i)
                     .isDeleted(false)
                     .build();
 
-            Item savedItem = itemRepository.save(item);
+            item.changeDescription("Description");
 
-            log.info(savedItem);
+            item.addImageString(UUID.randomUUID() + "_" + "IMAGE1.jpg");
+            item.addImageString(UUID.randomUUID() + "_" + "IMAGE2.jpg");
+
+            itemRepository.save(item);
+
         }
 
     }
@@ -50,10 +61,12 @@ public class ItemRepositoryTest {
     }
 
     @Test
-    public void testSearch() throws Exception {
+    public void getList() throws Exception {
 
-        itemRepository.searchTest();
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().build();
+
+        log.info(itemService.getList(pageRequestDTO));
 
     }
-
+    
 }
