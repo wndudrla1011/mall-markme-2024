8d08033eef7257b13714d5d605d3cefd2af4445f
wndudrla1011 <wndudrla1011@naver.com>
2024-03-19 15:53:18 +0900

    specified a date format

---
+++ src/main/java/com/rootable/mallmarkme2024/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.rootable.mallmarkme2024.config;
+
+import com.rootable.mallmarkme2024.formatter.LocalDateFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new LocalDateFormatter());
+    }
+}
---
+++ src/main/java/com/rootable/mallmarkme2024/formatter/LocalDateFormatter.java
@@ -0,0 +1,25 @@
+package com.rootable.mallmarkme2024.formatter;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class LocalDateFormatter implements Formatter<LocalDate> {
+
+    //String -> LocalDate
+    @Override
+    public LocalDate parse(String text, Locale locale) throws ParseException {
+        return LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    //LocalDate -> String
+    @Override
+    public String print(LocalDate object, Locale locale) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd").format(object);
+    }
+
+}
+
