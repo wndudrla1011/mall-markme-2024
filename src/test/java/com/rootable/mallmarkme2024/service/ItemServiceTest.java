072ad0968f7dd15d7825793e45d08a9416c7dd0e
wndudrla1011 <wndudrla1011@naver.com>
2024-03-25 12:26:43 +0900

    testing register for Item

--- src/test/java/com/rootable/mallmarkme2024/service/ItemServiceTest.java
+++ src/test/java/com/rootable/mallmarkme2024/service/ItemServiceTest.java
@@ -1,14 +1,24 @@
 package com.rootable.mallmarkme2024.service;
 
+import com.rootable.mallmarkme2024.domain.Item;
+import com.rootable.mallmarkme2024.dto.ItemDTO;
 import com.rootable.mallmarkme2024.dto.PageRequestDTO;
+import com.rootable.mallmarkme2024.dto.PageResponseDTO;
+import com.rootable.mallmarkme2024.repository.ItemRepository;
 import lombok.extern.log4j.Log4j2;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 @Log4j2
 public class ItemServiceTest {
+
+    @Autowired
+    private ItemRepository itemRepository;
 
     @Autowired
     private ItemService itemService;
@@ -18,7 +28,42 @@ public class ItemServiceTest {
 
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().build();
 
-        log.info(itemService.getList(pageRequestDTO));
+        PageResponseDTO<ItemDTO> responseDTO = itemService.getList(pageRequestDTO);
+
+        log.info(responseDTO.getDtoList());
+
+    }
+
+    @Test
+    public void get() throws Exception {
+
+        Long id = 1L;
+
+        ItemDTO itemDTO = itemService.get(id);
+
+        log.info(itemDTO.getName());
+        log.info(itemDTO.getWriter());
+        log.info(itemDTO.getPrice());
+        log.info(itemDTO.getDescription());
+        log.info(itemDTO.getStock());
+
+    }
+
+    @Test
+    public void register() throws Exception {
+
+        ItemDTO itemDTO = ItemDTO.builder()
+                .name("Test Item")
+                .writer("Test Writer")
+                .price(10000)
+                .description("Testing")
+                .stock(3)
+                .build();
+
+        Long savedId = itemService.register(itemDTO);
+
+        Item item = itemRepository.findById(savedId).orElseThrow();
+        assertThat(item).isNotNull();
 
     }
 
