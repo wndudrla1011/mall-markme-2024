e4ef426e0b0bee55e4a0472440ec206945b090ac
wndudrla1011 <wndudrla1011@naver.com>
2024-03-26 12:31:02 +0900

    moved to menu add button

--- src/main/frontend/src/pages/items/ListPage.js
+++ src/main/frontend/src/pages/items/ListPage.js
@@ -1,14 +1,7 @@
-import React, { useCallback } from "react";
+import React from "react";
 import ListComponent from "../../components/items/ListComponent";
-import { useNavigate } from "react-router-dom";
 
 function ListPage() {
-  const navigate = useNavigate();
-
-  const handleClickAdd = useCallback(() => {
-    navigate({ pathname: "add" });
-  });
-
   return (
     <div>
       <ListComponent />
