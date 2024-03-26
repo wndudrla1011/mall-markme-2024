8ca58c12abe8983bc6a5a6570380d559509f0b54
wndudrla1011 <wndudrla1011@naver.com>
2024-03-26 12:30:30 +0900

    added global variable to host

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import root from "./router/root";
 
+export const API_SERVER_HOST = "http://localhost:8080";
+
 function App() {
   return <RouterProvider router={root} />;
 }
