826df0d5d4c7457a3c456c054bb9aeaeb5fbf286
wndudrla1011 <wndudrla1011@naver.com>
2024-03-26 10:46:32 +0900

    routing to list page

--- src/main/frontend/src/router/itemRouter.js
+++ src/main/frontend/src/router/itemRouter.js
@@ -1,5 +1,24 @@
+import { Suspense, lazy } from "react";
+import Loading from "./../pages/Loading";
+import { Navigate } from "react-router-dom";
+
+const ItemList = lazy(() => import("../pages/items/ListPage"));
+
 const itemRouter = () => {
-  return [{}];
+  return [
+    {
+      path: "list",
+      element: (
+        <Suspense fallback={<Loading />}>
+          <ItemList />
+        </Suspense>
+      ),
+    },
+    {
+      path: "",
+      element: <Navigate replace={true} to={"list"} />,
+    },
+  ];
 };
 
 export default itemRouter;
