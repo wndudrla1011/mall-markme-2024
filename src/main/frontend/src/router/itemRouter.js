fa37b251a9fd968752c61850d95eecc843c1cff8
wndudrla1011 <wndudrla1011@naver.com>
2024-03-26 12:06:57 +0900

    created and routed add page for item

--- src/main/frontend/src/router/itemRouter.js
+++ src/main/frontend/src/router/itemRouter.js
@@ -3,6 +3,7 @@ import Loading from "./../pages/Loading";
 import { Navigate } from "react-router-dom";
 
 const ItemList = lazy(() => import("../pages/items/ListPage"));
+const ItemAdd = lazy(() => import("../pages/items/AddPage"));
 
 const itemRouter = () => {
   return [
@@ -17,6 +18,14 @@ const itemRouter = () => {
     {
       path: "",
       element: <Navigate replace={true} to={"list"} />,
+    },
+    {
+      path: "add",
+      element: (
+        <Suspense fallback={<Loading />}>
+          <ItemAdd />
+        </Suspense>
+      ),
     },
   ];
 };
