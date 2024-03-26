189ee4d6cc77314ece8cb5e7f094b9d2d0f159f3
wndudrla1011 <wndudrla1011@naver.com>
2024-03-26 11:45:13 +0900

    added list page for item

---
+++ src/main/frontend/src/components/items/ListComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ListComponent() {
+  return <div className="text-white">ListComponent</div>;
+}
+
+export default ListComponent;
--- src/main/frontend/src/pages/items/ListPage.js
+++ src/main/frontend/src/pages/items/ListPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ListComponent from "../../components/items/ListComponent";
 
 function ListPage() {
-  return <div className="text-3xl">ListPage</div>;
+  return (
+    <div>
+      <ListComponent />
+    </div>
+  );
 }
 
 export default ListPage;
