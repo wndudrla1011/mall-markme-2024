9672d714ee6e79bc81463adb9f6027b934de99bd
wndudrla1011 <wndudrla1011@naver.com>
2024-03-27 12:39:45 +0900

    activated Jpa Auditing

--- src/main/java/com/rootable/mallmarkme2024/MallMarkMe2024Application.java
+++ src/main/java/com/rootable/mallmarkme2024/MallMarkMe2024Application.java
@@ -3,7 +3,9 @@ package com.rootable.mallmarkme2024;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //Jpa Auditing 활성화
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MallMarkMe2024Application {
 
