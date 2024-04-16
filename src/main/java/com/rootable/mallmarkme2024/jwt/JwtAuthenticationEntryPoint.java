9d94709163a92beb7efd97673158cc4c63bac03c
wndudrla1011 <wndudrla1011@naver.com>
2024-04-16 14:20:47 +0900

    created JwtAuthenticationEntryPoint for handling 401 error

---
+++ src/main/java/com/rootable/mallmarkme2024/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.rootable.mallmarkme2024.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        //유효한 자격증명을 제공하지 않고 접근 시 401 에러
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+
+    }
+
+}
