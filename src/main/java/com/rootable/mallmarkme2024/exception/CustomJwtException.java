7bb05e864398b64c207f689de1b6cac149512ef5
wndudrla1011 <wndudrla1011@naver.com>
2024-04-16 20:00:04 +0900

    created CustomJwtException

---
+++ src/main/java/com/rootable/mallmarkme2024/exception/CustomJwtException.java
@@ -0,0 +1,9 @@
+package com.rootable.mallmarkme2024.exception;
+
+public class CustomJwtException extends RuntimeException {
+
+    public CustomJwtException(String message) {
+        super(message);
+    }
+
+}
