195af387c6b4459deaa8a1079f9e44e3b844e197
wndudrla1011 <wndudrla1011@naver.com>
2024-04-17 16:28:37 +0900

    edited SecurityConfig

--- src/main/java/com/rootable/mallmarkme2024/config/SecurityConfig.java
+++ src/main/java/com/rootable/mallmarkme2024/config/SecurityConfig.java
@@ -5,13 +5,13 @@ import com.rootable.mallmarkme2024.handler.JwtAuthenticationEntryPoint;
 import com.rootable.mallmarkme2024.handler.LoginFailureHandler;
 import com.rootable.mallmarkme2024.handler.LoginSuccessHandler;
 import com.rootable.mallmarkme2024.jwt.JwtFilter;
-import com.rootable.mallmarkme2024.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,13 +26,11 @@ import java.util.List;
 
 @Configuration
 @Log4j2
-@EnableMethodSecurity
 @RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
-    private final TokenProvider tokenProvider;
-
-    //PasswordEncoder
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -48,6 +46,9 @@ public class SecurityConfig {
             httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource());
         });
 
+        //세션 Disable
+        http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         http.csrf(AbstractHttpConfigurer::disable);
 
         //로그인 결과 핸들링
@@ -57,17 +58,14 @@ public class SecurityConfig {
             config.failureHandler(new LoginFailureHandler());
         });
 
+        //필터 등록
+        http.addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
+
         //예외 핸들링
         http.exceptionHandling(config -> {
             config.accessDeniedHandler(new JwtAccessDeniedHandler());
             config.authenticationEntryPoint(new JwtAuthenticationEntryPoint());
         });
-
-        //세션 Disable
-        http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        //필터 등록
-        http.addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
 
@@ -78,7 +76,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:300"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
